20ad02e5d0a46344d48e2c06852e7f44c0f494cf
joelbm00 <146150835+joelbm00@users.noreply.github.com>
2025-07-10 14:43:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
+import cors from 'cors';
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const cors = require("cors");
+const app = cors();
 app.use(cors()); // Sin restricciones
 
 const {
